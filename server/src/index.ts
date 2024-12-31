2561339545ff1cfc04a7a42eb545257d2c1d37aa
Ashutosh <kumarashutosh1956@gmail.com>
2024-12-31 16:51:55 +0530

    Rate limiting addedd

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,30 @@
 import express from "express";
+import rateLimit from 'express-rate-limit';
 const app = express();
 app.use(express.json());
+
+
+const otpRateLimiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 1 window - 5 minutes  
+    max: 3, // Limit each IP to 3 OTP requests per windowMs
+    message: 'Too many requests, please try again after 5 minutes',
+    standardHeaders: true, 
+    legacyHeaders: false
+})
+
+const resetPasswordLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000, 
+    max: 4, 
+    message: 'Too many requests, please try again after 10 minutes',
+    standardHeaders: true, 
+    legacyHeaders: false
+})
 
 //Let's cteate an in-memory object to store the otp
 
 const otpStore : Record<string,string> = {};
 
-app.post('/generate-otp',(req,res):any=>{
+app.post('/generate-otp',otpRateLimiter,(req,res):any=>{
     const email = req.body.email;
     if(!email){
         return res.status(400).json({
@@ -22,7 +40,7 @@ app.post('/generate-otp',(req,res):any=>{
 
 //Reset-Password
 
-  app.post('/password-reset', (req, res):any => {
+  app.post('/password-reset',resetPasswordLimiter, (req, res):any => {
     const { email, otp, newPassword } = req.body;
     if (!email || !otp || !newPassword) {
       return res.status(400).json({ message: "Email, OTP, and new password are required" });
